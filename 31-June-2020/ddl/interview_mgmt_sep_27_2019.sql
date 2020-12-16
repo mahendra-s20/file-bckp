a988a21075d40167ac57fdb3cfb565f8f02654f2
Sameer05515 <sameer.geeee@gmail.com>
2020-12-16 22:22:33 +0530

    committing at 16-12-2020 22:20:05.93

--- 31-June-2020/ddl/interview_mgmt_sep_27_2019.sql
+++ 31-June-2020/ddl/interview_mgmt_sep_27_2019.sql
@@ -99,4 +99,4 @@ CREATE TABLE `t_catg_ques_read_history` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-10 22:52:01
+-- Dump completed on 2020-12-16 22:19:50
--- 31-June-2020/ddl/topic_mgmt.sql
+++ 31-June-2020/ddl/topic_mgmt.sql
@@ -197,4 +197,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-10 22:51:52
+-- Dump completed on 2020-12-16 22:19:37
