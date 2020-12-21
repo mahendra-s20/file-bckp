57fa15052561b6ea4a1ed1f7b7daf2037bfd714f
Sameer05515 <sameer.geeee@gmail.com>
2020-12-21 16:30:44 +0530

    committing at 21-12-2020 16:28:36.29

--- Remembrable/my-sql-queries.sql
+++ Remembrable/my-sql-queries.sql
@@ -168,3 +168,113 @@ INSERT INTO `coding_ex_db`.`t_employee`
 	'125000'
 	)
 ------------------------------
+
+--===================================================
+SELECT tc.cat_id,tc.cat_name,tc.creation_date,tc.last_updation_date,tc.rating,last_read_date, 
+(SELECT COUNT(*) FROM t_catg_ques tcq WHERE tcq.linked_cat_id=cat_id) AS 'totalQuestionsCount' 
+FROM t_category tc WHERE cat_id='1'
+
+----------------------------
+
+INSERT INTO `parent_child_relation_topics`.`t_views` 
+(`title`, 	`parent_id` 	)
+VALUES
+('AWS',5 	) -- 33
+,('Jenkins',5 	)-- 34
+,('Jira',5) -- 35
+,('Version Control',5) -- 36
+,('Deployment',5 	)-- 37
+,('OS/File Transfer',5) -- 38
+,('Other Programming Language',5) -- 39
+,('Agile',5 	)-- 40
+;
+
+INSERT INTO `parent_child_relation_topics`.`t_views` 
+(`title`, 	`parent_id` 	)
+VALUES
+('GIT',36 	) -- 41
+,('SVN',36 	) -- 42
+,('Tomcat',37 	) -- 43
+,('Weblogic',37 	) -- 44
+,('WebSphere',37 	) -- 45
+,('Putty',38 	) -- 46
+,('WinSCP',38 	) -- 47
+,('Linux',38 	) -- 48
+,('Windows',38 	) -- 49
+,('Python',30 	) -- 50
+,('Ruby',39 	) -- 51
+;
+
+-----------------------
+
+SELECT
+    id, title
+FROM
+    t_views
+WHERE
+    parent_id IS NULL;
+    
+    
+SELECT
+    id, title
+FROM
+    t_views
+WHERE
+    parent_id = 1;
+	
+--------
+
+
+WITH RECURSIVE category_path (id, title,parent_id, PATH) AS
+(
+  SELECT id, title,parent_id, title AS PATH
+    FROM t_views
+    WHERE parent_id IS NULL
+  UNION ALL
+  SELECT c.id, c.title,c.parent_id, CONCAT(cp.path, ' > ', c.title)
+    FROM category_path AS cp JOIN t_views AS c
+      ON cp.id = c.parent_id
+)
+SELECT * FROM category_path
+ORDER BY PATH;
+
+-----------------------
+
+SELECT id, title,parent_id, title AS PATH
+    FROM t_views
+    WHERE parent_id IS NULL
+	
+-----------------------
+
+
+INSERT INTO `coding_ex_db`.`t_employee` 
+	(`EmployeeID`, 
+	`EmployeeName`, 
+	`Title`, 
+	`BusinessUnit`, 
+	`Place`, 
+	`SupervisorID`, 
+	`Competencies`, 
+	`Salary`
+	)
+	VALUES
+	('19', 
+	'Emp16', 
+	'Mrs.', 
+	'BU2', 
+	'Gurgaon', 
+	2, 
+	'COMP2', 
+	'135000'
+	),
+	('20', 
+	'Emp17', 
+	'Mrs.', 
+	'BU2', 
+	'Gurgaon', 
+	2, 
+	'COMP2', 
+	'125000'
+	)
+------------------------------
+--=======================================
