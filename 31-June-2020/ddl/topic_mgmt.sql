7d4233e24d0767afa3aa20f285b7f6b6f8b6e41a
Sameer05515 <sameer.geeee@gmail.com>
2020-12-25 20:59:10 +0530

    committing at 25-12-2020 20:56:46.89

--- 31-June-2020/ddl/topic_mgmt.sql
+++ 31-June-2020/ddl/topic_mgmt.sql
@@ -197,4 +197,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-25 20:20:50
+-- Dump completed on 2020-12-25 20:56:26
