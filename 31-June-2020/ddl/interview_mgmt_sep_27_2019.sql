25a62f5811d86a1b183e61d35175a4970bc27d44
Sameer05515 <sameer.geeee@gmail.com>
2020-12-26 14:28:44 +0530

    committing at 26-12-2020 14:26:17.82

--- 31-June-2020/ddl/interview_mgmt_sep_27_2019.sql
+++ 31-June-2020/ddl/interview_mgmt_sep_27_2019.sql
@@ -99,4 +99,4 @@ CREATE TABLE `t_catg_ques_read_history` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-25 20:56:34
+-- Dump completed on 2020-12-26 14:26:02
