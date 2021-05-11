b0b0774e7e3f66abde12f0c4d80bcc40068ae91e
Premendra.Kumar <premendra.bce05515@gmail.com>
2021-05-11 13:43:07 +0530

    committing at Tuesday 11 May 2021 01:41:12 PM IST

--- linux-db-backup/ddl/topic-mgmt.sql
+++ linux-db-backup/ddl/topic-mgmt.sql
@@ -197,4 +197,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-10 18:45:29
+-- Dump completed on 2021-05-11 13:40:56
