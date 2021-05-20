353a4eaccb0bae52eb7049e9341bbce65b4559d9
Premendra.Kumar <premendra.bce05515@gmail.com>
2021-05-20 17:01:42 +0530

    committing at Thursday 20 May 2021 05:00:47 PM IST

--- linux-db-backup/ddl/topic-mgmt.sql
+++ linux-db-backup/ddl/topic-mgmt.sql
@@ -70,7 +70,7 @@ CREATE TABLE `topic` (
   `last_read_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `uniquestrid` text,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=522 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=523 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -197,4 +197,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-11 13:40:56
+-- Dump completed on 2021-05-20 17:00:23
