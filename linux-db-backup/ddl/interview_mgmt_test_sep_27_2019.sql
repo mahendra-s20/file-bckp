bea18db9bc11985a89defa0d1c010a715cc7f4ea
Premendra.Kumar <premendra.bce05515@gmail.com>
2021-07-21 17:04:44 +0530

    committing at Wednesday 21 July 2021 05:03:39 PM IST

--- linux-db-backup/ddl/interview_mgmt_test_sep_27_2019.sql
+++ linux-db-backup/ddl/interview_mgmt_test_sep_27_2019.sql
@@ -99,4 +99,4 @@ CREATE TABLE `t_catg_ques_read_history` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-11 14:22:24
+-- Dump completed on 2021-07-21 17:03:03
