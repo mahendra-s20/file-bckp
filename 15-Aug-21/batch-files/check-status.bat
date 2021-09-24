4096a652be30acd62f313144af708506011200e7
Premendra Kumar <kumar.premendra@indiabulls.com>
2021-09-24 16:53:19 +0530

    committing at 24-09-2021 16:53:07.32

--- 15-Aug-21/batch-files/check-status.bat
+++ 15-Aug-21/batch-files/check-status.bat
@@ -14,8 +14,8 @@ echo time %myTime%
 
 echo %GIT_DUMP_DIR%\file-bckp
 
-echo "====    ANGULAR-NPM-PROJECTS   ================"
-pushd %GIT_PROJ_DIR%\angular-npm-projects && call git pull &&  call git status && call git add . && call git commit -a -m "committing at %myDate% %myTime%" && call git push && popd
+echo "====    ANGULAR-NPM-PROJECTS1   ================"
+pushd %GIT_PROJ_DIR%\angular-npm-projects1 && call git pull &&  call git status && call git add . && call git commit -a -m "committing at %myDate% %myTime%" && call git push && popd
 
 echo "====    FILE-BCKP   ================"
 pushd %GIT_DUMP_DIR%\file-bckp && call git pull &&  call git status && call git add . && call git commit -a -m "committing at %myDate% %myTime%" && call git push && popd
@@ -27,7 +27,7 @@ echo "== GIT_MY_PAGES_APPS_DIR =="
 pushd %GIT_MY_PAGES_APPS_DIR% && call git pull &&  call git status && call git add . && call git commit -a -m "committing at %myDate% %myTime%" && call git push && popd
 
 echo "==== Logs of current commits ================"
-pushd %GIT_PROJ_DIR%\angular-npm-projects && echo "===============================" && git remote show origin && call git log -1  && popd
+pushd %GIT_PROJ_DIR%\angular-npm-projects1 && echo "===============================" && git remote show origin && call git log -1  && popd
 pushd %GIT_DUMP_DIR%\file-bckp && echo "===============================" && git remote show origin && call git log -1 && popd
 pushd %GIT_DUMP_DIR%\MyTestPrograms && echo "===============================" && git remote show origin && call git log -1 && popd
 pushd %GIT_MY_PAGES_APPS_DIR% && echo "===============================" && git remote show origin && call git log -1  && popd
